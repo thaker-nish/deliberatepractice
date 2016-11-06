1986cd30e2c9e84815df7aa5689457b6cd37a4a8
thaker-nish <thaker.nish@gmail.com>
2016-11-06 14:36:44 -0600

    Initial commit

---
+++ put_numbers_in_order.rb
@@ -0,0 +1,5 @@
+# using the array [5, 3, 10 , 18, 22, 4, 5, 11].
+# Print the numbers in order from lowest to & higest to lowest.
+numbers = [5, 3, 10, 18, 22, 4, 5, 11]
+p numbers.sort
+p numbers.sort.reverse
